ec8b574590cc13fce246bfdfeac2802ae3af3feb
BOGE-TA <bogumjed@gmail.com>
2024-10-05 14:43:30 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,8 @@
-public class HelloWorld {
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Bogumila");
+        System.out.println(31);
+        System.out.println('G');// [6]
+    }
 }
