b63a2a382b457f0bc061c37c29c30059d7080fd6
Bogumila Gehring <bogumjed@gmail.com>
2024-11-04 17:38:54 +0100

    changes 4.11, 17:38

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,11 +1,22 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String example = "Hello from FirstClass!";
-        System.out.println(example);
-        if (1>2) {
-            System.out.println ("Niesamowite! Jeden jest wieksze niz dwa!!!");
-        } else {
-            System.out.println ("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        Notebook notebook = new Notebook(600, 1000);
+        System.out.println("notebook: " + notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook (2000, 1500);
+        System.out.println("heavyNotebook.weight: " + heavyNotebook.weight);
+        System.out.println("heavyNotebook.price: " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook (1600, 500);
+        System.out.println("oldNotebook.weight: " + oldNotebook.weight);
+        System.out.println("oldNotebook.price: " + oldNotebook.price);
+        oldNotebook.checkPrice();
+        if (notebook.price < 900) {
+            System.out.println("This notebook is cheap.");
+        } else{
+            System.out.println("This notebook is quite expensive.");
         }
     }
 }
