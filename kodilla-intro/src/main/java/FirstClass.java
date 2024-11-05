cb66cb94a53e878ae906178c2bbc490d7fa6a7e7
Bogumila Gehring <bogumjed@gmail.com>
2024-11-05 20:54:09 +0100

    zmiany 5.11

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,22 +1,25 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600, 1000);
-        System.out.println("notebook: " + notebook.weight + " " + notebook.price);
+        Notebook notebook = new Notebook(600, 1000, 2023);
+        System.out.println("notebook weight: " + notebook.weight + "," + " " + "notebook price: " + notebook.price + "," + " " + "notebook year: " + notebook.year);
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
 
-        Notebook heavyNotebook = new Notebook (2000, 1500);
+        Notebook heavyNotebook = new Notebook (2000, 1500, 2024);
         System.out.println("heavyNotebook.weight: " + heavyNotebook.weight);
         System.out.println("heavyNotebook.price: " + heavyNotebook.price);
+        System.out.println("heavyNotebook.year: " + heavyNotebook.year);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
 
-        Notebook oldNotebook = new Notebook (1600, 500);
+        Notebook oldNotebook = new Notebook (1600, 500, 2020);
         System.out.println("oldNotebook.weight: " + oldNotebook.weight);
         System.out.println("oldNotebook.price: " + oldNotebook.price);
+        System.out.println("oldNotebook.year: " + oldNotebook.year);
         oldNotebook.checkPrice();
-        if (notebook.price < 900) {
-            System.out.println("This notebook is cheap.");
-        } else{
-            System.out.println("This notebook is quite expensive.");
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
         }
     }
-}
