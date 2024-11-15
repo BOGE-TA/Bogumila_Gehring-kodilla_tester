da1748a2d42a628955aa1bfe1b7a18821eb45ad2
Bogumila Gehring <bogumjed@gmail.com>
2024-11-15 14:45:27 +0100

    zmiany 15.11

---
+++ kodilla-intro/src/main/java/UserDialogs.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class UserDialogs {
+    public static String getUsername() {
+        Scanner scanner = new Scanner (System.in);      // [1]
+
+        while (true) {                                  // [2]
+
+            System.out.println("Enter your name:");    // [3]
+
+            String name = scanner.nextLine().trim();    // [4]
+
+            if (name.length() >= 2) {                    // [5]
+
+                return name;                            // [6]
+            }
+            System.out.println("Name is too short. Try again.");
+        }
+    }
+    public static String getUserSelection() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Select calculation (A-add, S-substract, D-divide, M-multiply):");
+            String calc = scanner.nextLine().trim().toUpperCase();    // [1]
+            switch (calc) {
+                case "A":
+                    return "ADD";
+                case "S":
+                    return "SUB";
+                case "D":
+                    return "DIV";
+                case "M":
+                    return "MUL";
+                default:
+                    System.out.println("Wrong calculation. Try again.");
+            }
+        }
+    }
