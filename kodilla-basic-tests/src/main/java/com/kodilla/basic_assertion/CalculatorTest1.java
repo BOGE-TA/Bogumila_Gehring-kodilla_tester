4228c92f075a33ee49470c787bff877737b61b7f
Bogumila Gehring <bogumjed@gmail.com>
2024-11-27 21:06:37 +0100

    changes 21:06

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/CalculatorTest1.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/CalculatorTest1.java
@@ -1,11 +1,34 @@
 package com.kodilla.basic_assertion;
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CalculatorTest {
+public class CalculatorTest1 {
+
+        @Test
+        public void testSum() {
+            // Given
+            Calculator calculator = new Calculator();
+
+            // When
+            int result = calculator.sum(5, 3);
+
+            // Then
+            assertEquals(String.valueOf(8), result, "Sum of 5 and 3 should be 8");
+        }
+
+        @Test
+        public void testSubtract() {
+            // Given
+            Calculator calculator = new Calculator();
+
+            // When
+            int result = calculator.subtract(10, 4);
+
+            // Then
+            assertEquals(String.valueOf(6), result, "Subtraction of 10 and 4 should be 6");
+        }
 
         @Test
         public void testSquareWithPositiveNumber() {
@@ -25,10 +48,10 @@ public class CalculatorTest {
             Calculator calculator = new Calculator();
 
             // When
-            int result = calculator.square(-4);
+            int result = calculator.square(-3);
 
             // Then
-            assertEquals(String.valueOf(16), result, "Square of -4 should be 16");
+            assertEquals(String.valueOf(9), result, "Square of -3 should be 9");
         }
 
         @Test
@@ -42,5 +65,5 @@ public class CalculatorTest {
             // Then
             assertEquals(String.valueOf(0), result, "Square of 0 should be 0");
         }
-
     }
+
