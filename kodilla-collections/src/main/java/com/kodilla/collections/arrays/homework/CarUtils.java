c9524b9b6cd9c3e06110ac68f07a153818c60d1f
Bogumila Gehring <bogumjed@gmail.com>
2024-12-06 21:41:25 +0100

    changes 06.12, 21:41

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -1,4 +1,22 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+
 public class CarUtils {
+    public static void describeCar(Car car) {
+        System.out.println("Car make: " + getCarName(car));
+        System.out.println("Current speed: " + car.getSpeed());
+    }
+
+    private static String getCarName(Car car) {
+        if (car instanceof com.kodilla.collections.interfaces.homework.Ford) {
+            return "Ford";
+        } else if (car instanceof com.kodilla.collections.interfaces.homework.Opel) {
+            return "Opel";
+        } else if (car instanceof com.kodilla.collections.interfaces.homework.Mazda) {
+            return "Toyota";
+        } else {
+            return "Unknown Car";
+        }
+    }
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -1,4 +1,52 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Mazda;
+import com.kodilla.collections.interfaces.homework.Opel;
+import com.kodilla.collections.interfaces.homework.Mazda;
+
+import java.util.Random;
+
 public class CarsApplication {
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        int arraySize = RANDOM.nextInt(15) + 1; // Random size from 1 to 15
+        Car[] cars = new Car[arraySize];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = drawCar();
+        }
+
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
+            System.out.println("------------------");
+        }
+    }
+
+    public static Car drawCar() {
+        int carType = RANDOM.nextInt(3); // 0 - Ford, 1 - Opel, 2 - Mazda
+        Car car;
+        switch (carType) {
+            case 0:
+                car = new Ford();
+                break;
+            case 1:
+                car = new Opel();
+                break;
+            case 2:
+                car = new Mazda();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + carType);
+        }
+
+        // Random speed increase from 1 to 10 times
+        int speedIncreaseTimes = RANDOM.nextInt(10) + 1;
+        for (int i = 0; i < speedIncreaseTimes; i++) {
+            car.increaseSpeed();
+        }
+        return car;
+    }
 }
