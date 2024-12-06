f8c9fe435b627f8227af553d563380239e2de744
Bogumila Gehring <bogumjed@gmail.com>
2024-12-06 18:22:53 +0100

    changes 06.12.24

--- kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTest.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/bank/homework/BankTest.java
@@ -2,44 +2,44 @@ package com.kodilla.bank.homework;
 
 import com.kodilla.bank.homework.Bank;
 import com.kodilla.bank.homework.CashMachine;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-    public class BankTest {
+public class BankTest {
+    Bank bank;
 
-        @Test
-        public void testTotalBalance() {
-            Bank bank = new Bank(2);
-            bank.getCashMachine(0).addTransaction(200);
-            bank.getCashMachine(1).addTransaction(-100);
-
-            assertEquals(100, bank.getTotalBalance());
-        }
-
-        @Test
-        public void testTotalNumberOfDeposits() {
-            Bank bank = new Bank(2);
-            bank.getCashMachine(0).addTransaction(200);
-            bank.getCashMachine(1).addTransaction(300);
-
-            assertEquals(2, bank.getTotalNumberOfDeposits());
-        }
-
-        @Test
-        public void testAverageWithdrawal() {
-            Bank bank = new Bank(2);
-            bank.getCashMachine(0).addTransaction(-200);
-            bank.getCashMachine(1).addTransaction(-300);
-
-            assertEquals(-250, bank.getAverageWithdrawal());
-        }
-
-        @Test
-        public void testAverageDeposit() {
-            Bank bank = new Bank(2);
-            bank.getCashMachine(0).addTransaction(200);
-            bank.getCashMachine(1).addTransaction(300);
-
-            assertEquals(250, bank.getAverageDeposit());
-        }
+    @BeforeEach
+    pubic void setUp(){
+        bank = new Bank(2);
     }
+
+    @Test
+    public void testTotalBalance() {
+        addTransactions(200, -100);
+        assertEquals(100, bank.getTotalBalance());
+    }
+
+    @Test
+    public void testTotalNumberOfDeposits() {
+        addTransactions(200, 300);
+        assertEquals(2, bank.getTotalNumberOfDeposits());
+    }
+
+    @Test
+    public void testAverageWithdrawal() {
+        addTransactions(-200, -300);
+        assertEquals(-250, bank.getAverageWithdrawal());
+    }
+
+    @Test
+    public void testAverageDeposit() {
+        addTransactions(200, 300);
+        assertEquals(250, bank.getAverageDeposit());
+    }
+
+    public void addTransactions(int first, int second) {
+        bank.getCashMachine(0).addTransaction(first);
+        bank.getCashMachine(1).addTransaction(second);
+    }
+}
