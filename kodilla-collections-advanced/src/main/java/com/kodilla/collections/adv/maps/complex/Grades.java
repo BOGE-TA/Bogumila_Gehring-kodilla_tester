9ebc55264e7e7ce7eed290f798ac7e1ba524f701
Bogumila Gehring <bogumjed@gmail.com>
2024-12-09 22:44:56 +0100

    changes 09.12, 22:44

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/Grades.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/Grades.java
@@ -1,18 +1,28 @@
 package com.kodilla.collections.adv.maps.complex;
-import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Grades {
-    private List<Double> grades = new ArrayList<>();
+    private List<Double> grades;
 
-    public Grades(List<Double> grades) {
-        this.grades = grades;
+    public Grades(Double... grades) {
+        this.grades = Arrays.asList(grades);
     }
 
-    public double getAverage() { // [1]
-        double sum = 0.0; // [2]
-        for (double grade : grades) // [3]
-            sum += grade; // [4]
-        return sum / grades.size(); // [5]
+    public List<Double> getGrades() {
+        return grades;
+    }
+
+    public double getAverage() {
+        return grades.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    @Override
+    public String toString() {
+        return "Grades: " + grades.toString();
     }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/Student.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/complex/Student.java
@@ -6,25 +6,9 @@ public class Student {
     private String firstname;
     private String lastname;
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(firstname, student.firstname) && Objects.equals(lastname, student.lastname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstname, lastname);
+    public Student(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
     public String getFirstname() {
@@ -35,8 +19,17 @@ public class Student {
         return lastname;
     }
 
-    public Student(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstname, student.firstname) &&
+                Objects.equals(lastname, student.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
     }
 }
